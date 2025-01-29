bb5b757dda506919b5abc5b5744ba616c25aadd0
Suganame <gsuganame@gmail.com>
2025-01-29 20:34:46 -0300

    feat: add improvements and refactors

--- checkout/src/checkout/checkout.controller.ts
+++ checkout/src/checkout/checkout.controller.ts
@@ -7,12 +7,19 @@ export class CheckoutController {
 
   private readonly logger = new Logger(CheckoutService.name);
 
-  constructor(private readonly checkoutService: CheckoutService) {}
+  constructor(private readonly checkoutService: CheckoutService) { }
 
   @Post()
   @HttpCode(HttpStatus.OK)
-  create(@Body() createCheckoutDto: CreateCheckoutDto) {
+  execute(@Body() createCheckoutDto: CreateCheckoutDto) {
     this.logger.log(`Route /checkout accessed with followed data: body=${JSON.stringify(createCheckoutDto)}.`)
-    return this.checkoutService.checkout(createCheckoutDto);   
+    return this.checkoutService.checkout(createCheckoutDto);
+  }
+
+  @Post("/retry")
+  @HttpCode(HttpStatus.OK)
+  executeRetry(@Body() createCheckoutDto: CreateCheckoutDto) {
+    this.logger.log(`Route /checkout/retry accessed with followed data: body=${JSON.stringify(createCheckoutDto)}.`)
+    return this.checkoutService.checkoutRetry(createCheckoutDto);
   }
 }
--- checkout/src/checkout/checkout.service.ts
+++ checkout/src/checkout/checkout.service.ts
@@ -1,50 +1,72 @@
-import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConflictException, GatewayTimeoutException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import axios, { AxiosError } from 'axios';
-import axiosRetry from 'axios-retry';
+import { firstValueFrom } from 'rxjs';
 import { CreateCheckoutDto } from './dto/create-checkout.dto';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class CheckoutService {
 
   private readonly logger = new Logger(CheckoutService.name);
+  private readonly baseUrl;
 
-  constructor(private configService: ConfigService) { }
+  constructor(private configService: ConfigService, private readonly httpService: HttpService) {
+    if (!this.configService.get<string>('PAYMENT_GATEWAY_BASEURL') || this.configService.get<string>('PAYMENT_GATEWAY_BASEURL') === "") {
+      throw new InternalServerErrorException();
+    }
+    this.baseUrl = this.configService.get<string>('PAYMENT_GATEWAY_BASEURL');
+  }
 
   async checkout(createCheckoutDto: CreateCheckoutDto) {
-
-    let attemptCount: number = 0;
-
-    axios.interceptors.request.use((config) => {
-      attemptCount++;
-      this.logger.warn(`Something went wrong, trying again: Attempt number #${attemptCount}`);
-      return config;
-    });
-
-    const baseUrl = this.configService.get<string>('PAYMENT_GATEWAY_BASEURL');
-
-    axiosRetry(axios, {
-      retries: 3, retryDelay: (retryCount) => {
-        return axiosRetry.exponentialDelay(retryCount) * 3;
-      }
-    });
-
     try {
-      const { data } = await axios.post(`${baseUrl}/checkout/execute`, createCheckoutDto, {
+      const { data } = await firstValueFrom(this.httpService.post(`${this.baseUrl}/checkout/execute`, createCheckoutDto, {
         headers: {
           "Content-Type": "application/json"
         }
-      })
-
-      this.logger.verbose(`Operation done succesfully. response=${JSON.stringify(data)}`)
-
-      return data
+      }));
+      return data;
     } catch (error) {
-      if (error instanceof AxiosError) {
-        this.logger.error("After several attempts, the server returned an error. Please, Try again later.", new Error().stack)
-        throw new InternalServerErrorException("After several attempts, the server returned an error. Please, Try again later.");
+      if (error.code == "ECONNREFUSED") {
+        this.logger.error("Payment gateway unavaliable.", new Error().stack)
+        throw new InternalServerErrorException();
       }
+      if (error.status === 500) {
+        this.logger.error("After several attempts, the server returned an error.", new Error().stack)
+        throw new GatewayTimeoutException("After several attempts, the server returned an error. Please, contact our support.")
+      } else if (error.status === 409) {
+        this.logger.error(error.response.data, new Error().stack)
+        throw new ConflictException(error.response.data)
+      } else {
+        this.logger.error("Unknow error.", new Error().stack)
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 
+  async checkoutRetry(createCheckoutDto: CreateCheckoutDto) {
+    try {
+      const { data } = await firstValueFrom(this.httpService.post(`${this.baseUrl}/checkout/execute/retry`, createCheckoutDto, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }));
+      return data;
+    } catch (error) {
+      if (error.code == "ECONNREFUSED") {
+        this.logger.error("Payment gateway unavaliable.", new Error().stack)
+        throw new InternalServerErrorException();
+      }
+      if (error.status === 500) {
+        this.logger.error("After several attempts, the server returned an error.", new Error().stack)
+        throw new GatewayTimeoutException("After several attempts, the server returned an error. Please, contact our support.")
+      } else if (error.status === 409) {
+        this.logger.error(error.response.data, new Error().stack)
+        throw new ConflictException(error.response.data)
+      } else {
+        this.logger.error("Unknow error.", new Error().stack)
+        throw new InternalServerErrorException();
+      }
     }
   }
 }
