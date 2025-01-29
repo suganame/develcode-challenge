5799e14582adf1aa934aa004aea53a0d69d19b98
Suganame <gsuganame@gmail.com>
2025-01-29 20:34:53 -0300

    feat: add improvements and refactors

--- checkout/src/checkout/checkout.module.ts
+++ checkout/src/checkout/checkout.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CheckoutService } from './checkout.service';
+import { AxiosModule } from 'src/axios/axios.module';
 import { CheckoutController } from './checkout.controller';
+import { CheckoutService } from './checkout.service';
 
 @Module({
   controllers: [CheckoutController],
   providers: [CheckoutService],
+  imports: [AxiosModule]
 })
 export class CheckoutModule {}
